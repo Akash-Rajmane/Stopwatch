c7ef10b78c39bdb67f64cbda9fd559e3af13840c
Akash Rajmane <90460418+Akash-Rajmane@users.noreply.github.com>
2021-10-13 18:11:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,93 +1,76 @@
-var hr = 0;
-var min = 0;
-var sec = 0;
+let sec = 0;
+let min = 0;
+let hr = 0;
+let intervalID;
 
-var hrElement = document.getElementById("hr");
-var minElement = document.getElementById("min");
-var secElement = document.getElementById("sec");
+let secElement = document.getElementById("seconds");
+let minElement = document.getElementById("minutes");
+let hrElement = document.getElementById("hours");
 
-var startBtn = document.getElementById("start");
-var pauseBtn = document.getElementById("pause");
-var resetBtn = document.getElementById("stop");
+let startButton = document.getElementById("start");
+let pauseButton = document.getElementById("pause");
+let resetButton = document.getElementById("stop");
 
-var isTimeTicking = false; 
-
-function start(){
-
-    startBtn.disabled = true;
-    pauseBtn.disabled = false;
-    resetBtn.disabled = false;
-
-    if( isTimeTicking==false ){
-        isTimeTicking = true;
-        displayTime();
+function print(val) {
+    if (val <= 9) {
+        return "0" + val;
+    } else {
+        return val;
     }
 }
-
-function pause(){
-    if( isTimeTicking==true && pauseBtn.innerHTML=="pause" ){
-        isTimeTicking = false;
-        pauseBtn.innerHTML = "continue";
-    }
-    else if( isTimeTicking==false && pauseBtn.innerHTML=="continue" ){
-        isTimeTicking = true;
-        pauseBtn.innerHTML = "pause";
-        displayTime();
-    }
-}
-
-function reset(){
-    isTimeTicking = false;
-    hr = 0;
-    min = 0;
-    sec = 0;
-    hrElement.innerHTML = "00";
-    minElement.innerHTML = "00";
-    secElement.innerHTML = "00";
-    
-    startBtn.disabled = false;
-    pauseBtn.disabled = true;
-    resetBtn.disabled = true;
-
-    if( pauseBtn.innerHTML=="continue" ){
-        pauseBtn.innerHTML=="pause";
-    }
-}
-
 
 function displayTime() {
-    if( isTimeTicking==true ){
+    secElement.innerHTML = print(sec);
+    minElement.innerHTML = print(min);
+    hrElement.innerHTML = print(hr);
+}
 
-        sec++;
+function startTimer() {
+    sec++;
+    if (sec == 60) {
+        sec = 0;
+        min++;
+    } 
+    
+    if (min == 60) {
+        min = 0;
+        hr++;
+    } 
+    
+    displayTime();
+}
 
-        if( sec==59 ){
-            sec = 0;
-            min++;
-        }
+function start() {
+    startButton.disabled = true;
+    pauseButton.disabled = false;
+    resetButton.disabled = false;
+    intervalID = setInterval(startTimer, 1000);
+}
 
-        if( min==59 ){
-            min = 0;
-            hr++;
-        }
-
-        if( sec<10 ) {
-           sec = sec.toString().padStart(2,0);
-        }
-
-        if( min<10 ) {
-            min = min.toString().padStart(2,0);
-        }
-
-        if ( hr<10 ) {
-            hr = hr.toString().padStart(2,0);
-        }
-        
-        secElement.innerHTML = sec;
-        minElement.innerHTML = min;
-        hrElement.innerHTML = hr;
-
-        setTimeout("displayTime()", 1000);
-
+function pause() {
+    if (pauseButton.innerHTML == "pause") {
+        clearInterval(intervalID);
+        pauseButton.innerHTML = "continue";
+    } else {
+        pauseButton.innerHTML = "pause";
+        intervalID = setInterval(startTimer, 1000);
     }
+}
 
+function reset() {
+    clearInterval(intervalID);
+    sec = 0;
+    min = 0;
+    hr = 0;
+    secElement.innerHTML = "00";
+    minElement.innerHTML = "00";
+    hrElement.innerHTML = "00";
+
+    startButton.disabled = false;
+    pauseButton.disabled = true;
+    resetButton.disabled = true;
+
+    if (pauseButton.innerHTML == "continue") {
+        pauseButton.innerHTML = "pause";
+    }
 }
